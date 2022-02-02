de27cd90cb74d23a4e14a19f7ff9f47b4f906211
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-02-02 19:49:28 -0400

    Fix network images issue

---
+++ toc-clean.py
@@ -0,0 +1,19 @@
+import re
+
+
+def main():
+    content = ""
+    with open("intro.md") as f:
+        content = f.read()
+
+    pattern = r"<!--content:start-->(.*)((\s)+(.*))+<!--content:end-->"
+    no_toc = f"<!--content:start--><!--content:end-->"
+
+    content = re.sub(pattern, no_toc, content)
+
+    with open("intro.md", "w") as f:
+        f.write(content)
+
+
+if __name__ == "__main__":
+    main()
